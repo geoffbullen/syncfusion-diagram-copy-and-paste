53df6b4a4adb958cab55bacdb24259ba1d305ce1
Geoff Bullen <geoff.bullen@gmail.com>
2022-01-31 13:48:43 +1100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { DiagramComponent, NodeModel } from '@syncfusion/ej2-angular-diagrams';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  @ViewChild("diagram") 
+  public diagram: DiagramComponent;
+
   title = 'syncfusion-diagram-copy-and-paste';
+
+  public node: NodeModel = {
+    // Position of the node
+    offsetX: 250,
+    offsetY: 250,
+    // Size of the node
+    width: 100,
+    height: 100,
+    style: {
+      fill: '#6BA5D7',
+      strokeColor: 'white'
+    },
+  };
+
+  public created(args: Object): void {
+    //Add Node
+    this.diagram.add(this.node);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { DiagramModule } from '@syncfusion/ej2-angular-diagrams';
 
 import { AppComponent } from './app.component';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+  @NgModule({
+    declarations: [
+      AppComponent
+    ],
+    imports: [
+      BrowserModule,DiagramModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+  })
+  export class AppModule { }
